7c6ef3516907c840f2e2f7b1d5f471ae8b110802
FVI <test@examle.com>
2017-07-06 14:44:38 -0400

    update exe

--- Studio/st4.py
+++ Studio/st4.py
@@ -1,12 +1,65 @@
 import turtle
 alex=turtle.Turtle()
 def drawSquare(t,sz):
+    alex.forward(50)
+    alex.left(90)
+    alex.forward(50)
+    alex.left(90)
+    alex.forward(50)
+    alex.left(90)
+    alex.forward(50)
+    alex.left(90)
 
-        alex.forward(100)
-        alex.left(90)
+    alex.left(90)
 
-
+#
 for i in range(20):
     drawSquare(alex,40)
-    alex.forward(10)
     alex.left(5)
+#     alex.forward(10)
+#     alex.left(5)
+# for i in range(4):
+#     drawSquare(alex,20)
+#
+# alex.left(20
+# for i in range(4):
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+#
+# alex.left(90)
+#
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+#
+# alex.left(90)
+#
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.left(90)
+#
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
+# alex.forward(50)
+# alex.left(90)
